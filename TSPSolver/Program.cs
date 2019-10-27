5cedb70d09b2b4246909cf726b9d1dacf6d0b70f
Armend Ukehaxhaj <armendd.u@hotmail.com>
2019-10-27 19:47:12 +0100

    Initial commit

---
+++ TSPSolver/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TSPSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("./input/p01_d.txt").ToList();
+
+            var citiesInformation = new string[lines.Count][];
+            // Initialize arrays
+            for (int i = 0; i < citiesInformation.Length; i++)
+            {
+                citiesInformation[i] = new string[lines.Count];
+            }
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                List<string> strippedLines = lines[i].Split(' ').Where(l => l != "").ToList();
+                for (int j = 0; j < strippedLines.Count; j++)
+                {
+                    citiesInformation[i][j] = strippedLines[j];
+                }
+            }
+
+            var solver = new TspSolver(citiesInformation);
+            var result = solver.Solve();
+            Console.WriteLine($"The sum of the result is {result.Item1}");
+            Console.WriteLine("The array of cities is:");
+            foreach (var item in result.Item2)
+            {
+                Console.Write($"{item},");
+            }
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TSPSolver/TspSolver.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace TSPSolver
+{
+    class TspSolver
+    {
+        public static int MAX_ITERATIONS = 50000;
+        public static Random _random = new Random();
+        private string[][] _citiesInfo;
+
+        public TspSolver(string[][] citiesInformation)
+        {
+            _citiesInfo = citiesInformation;
+        }
+
+        private int[] GenerateRandomSolution()
+        {
+            var randomList = Enumerable.Range(0, 15).ToList();
+
+            return randomList.Shuffle(_random).ToArray<int>();
+        }
+
+        private int GetDistance(int city1, int city2)
+        {
+            return int.Parse(_citiesInfo[city1][city2]);
+        }
+
+        public (int, int[]) Solve()
+        {
+            var sumResult = 1000;
+            var solutionList = new int[_citiesInfo.Length];
+            int[] solution = new int[_citiesInfo.Length];
+            int i = 0;
+
+            while (i < MAX_ITERATIONS)
+            {
+                solution = GenerateRandomSolution();
+
+                var resultOfSolution = 0;
+
+                for (int j = 0; j < solution.Length; j++)
+                {
+                    if (j != solution.Length - 1)
+                        resultOfSolution += GetDistance(solution[j], solution[j + 1]);
+                    else
+                        resultOfSolution += GetDistance(solution[j], solution[0]);
+                }
+
+                if (resultOfSolution < sumResult)
+                {
+                    sumResult = resultOfSolution;
+                    solutionList = solution;
+                }
+                else
+                    i++;
+            }
+
+            return (sumResult, solutionList);
+        }
+    }
+}
